30898ea380e1627ebddc4411a381b47892647ce6
JhonasRM <129074169+JhonasRM@users.noreply.github.com>
2024-03-11 21:45:11 -0300

    Revert "'init'"

--- src/Service/Repositories/IUser.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../Model/User";
-
-export interface IUsersRepository{
-    findByEmail(email: string): Promise<User>;
-    save(user: User): Promise<void>
-}
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./Presentation/View/app"
-
-const port = 2297
-
-app.listen(port, () => {
-    console.log(`O servidor está online na porta ${port}`)
-})
