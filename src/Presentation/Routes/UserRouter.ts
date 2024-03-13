cdf89a59b0c124727a5da8be8e4eea33e9b81692
JohnDias0 <dias44520@gmail.com>
2024-03-13 18:14:27 -0300

    feat: router para CreateUser e implementação de testes unitários

--- src/Presentation/Routes/UserRouter.ts
+++ src/Presentation/Routes/UserRouter.ts
@@ -1,25 +1,26 @@
-import { Router, Request, Response } from 'express';
-import { UserController } from "../Controller/UserController";
+import { Router, Request, Response} from 'express';
+import { CreateUserController } from '../UseCases/Create/CreateUserController';
+const createUserController = require('../UseCases/Create/CreateUserUC');
 
 const router: Router= Router();
-
 router
     .route("/user")
-    .post((req: Request ,res: Response) => UserController.create(req, res))
+    .post((req: Request ,res: Response) => createUserController.handle(req, res))
 
-router
-    .route("/users")
-    .get((req: Request ,res: Response) => UserController.getAll(req, res))
+
+// router
+//     .route("/users")
+//     .get((req: Request ,res: Response) => UserController.getAll(req, res))
     
-router
-    .route("/user")
-    .get((req: Request ,res: Response) => UserController.get(req, res))
+// router
+//     .route("/user")
+//     .get((req: Request ,res: Response) => UserController.get(req, res))
 
- router
-    .route("/user")
-    .delete((req: Request ,res: Response) => UserController.delete(req, res))
+//  router
+//     .route("/user")
+//     .delete((req: Request ,res: Response) => UserController.delete(req, res))
 
-router
-    .route("/user")
-    .put((req: Request, res: Response)=> UserController.update(req,res))
+// router
+//     .route("/user")
+//     .put((req: Request, res: Response)=> UserController.update(req,res))
 module.exports = router;
--- src/Presentation/UseCases/Create/CreateUserUC.ts
+++ src/Presentation/UseCases/Create/CreateUserUC.ts
@@ -1,16 +1,16 @@
 import { User } from "../../../Service/Model/User";
-import { IUsersRepository } from "../../../Service/Repositories/IUser";
+import { UsersRepository } from "../../../Service/Repositories/UsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 export class CreateUserUC {
-    constructor(private usersRepository: IUsersRepository) { }
+    constructor(private usersRepository: UsersRepository) { }
     async execute(data: ICreateUserRequestDTO) {
         const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
 
-        if (userAlreadyExists) {
-            throw new Error('User already exists')
-        }
-       
-        const user: User = new User(data)
-        await this.usersRepository.save(user)
+        if (userAlreadyExists !== null) {
+            throw new Error('O Usuário já existe')
+        } 
+        const NewUser: User = new User(data)
+        console.log('Cadastrando novo Usuário')
+        await this.usersRepository.save(NewUser)
     }
 }
