3d48a4f66b3413b61e3a23597714e55f3db0080d
JohnDias0 <dias44520@gmail.com>
2024-03-21 16:05:58 -0300

    fix: ReadAllUserUC.test.ts teste implementado e revisado / feat: PathIgnorePatterns no jest.config.js - testes já implementados e revisados não serão rodados novamente durante os testes unitários

--- src/Presentation/UseCases/ReadAllUsers/ReadAllUserUC.test.ts
+++ src/Presentation/UseCases/ReadAllUsers/ReadAllUserUC.test.ts
@@ -1,15 +1,16 @@
 import { User } from "../../../Service/Model/User"
 import { UsersRepository } from "../../../Service/Repositories/UsersRepository"
 
-describe('UserRepository', () => {
+describe('UserRepository function getAllUsers to return a Array List of Users from Firebase', () => {
     let userRepository: UsersRepository
 
     beforeAll(() => {
-        userRepository = new UsersRepository()
-    })
+        userRepository = new UsersRepository();
+    });
 
     test('getAllUsers should return an Array of type Users', async () => {
-        const Users = await userRepository.getAllUsers()
-        expect(Users).toContain(User)
-    })
+        const Users = await userRepository.getAllUsers();
+        expect(Users).toBeInstanceOf(Array);
+        expect(Users?.length).toBeGreaterThan(0);
+    }, 10000);
 })
