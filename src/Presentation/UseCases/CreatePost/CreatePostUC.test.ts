486c6d1ef35be18e457b02298596ec56a9fc7340
JohnDias0 <dias44520@gmail.com>
2024-04-03 17:39:07 -0300

    feat: CreateUser.test.ts - search for a non existing post

--- src/Presentation/UseCases/CreatePost/CreatePostUC.test.ts
+++ src/Presentation/UseCases/CreatePost/CreatePostUC.test.ts
@@ -14,16 +14,21 @@ describe('UserRepository', () => {
     // conn()
 });
 
-  test('Create Post without user verification should return by console.log the post data', async () => {
-        const PostData: Post = new  Post({
-            'description': 'NewPost',
-            'local': 'Etec Zona Leste',
-            'status': 0,
-            // 'UserID': user?.id,
-            'createdAt': Date.now()
-        })
-        const NewPost = await postRepository.save(PostData)
-    expect(NewPost).not.toBeNull()
-  }, 100000);
-  
+  // test('Create Post without user verification should return by console.log the post data', async () => {
+  //       const PostData: Post = new  Post({
+  //           'description': 'NewPost',
+  //           'local': 'Etec Zona Leste',
+  //           'status': 0,
+  //           // 'UserID': user?.id,
+  //           'createdAt': Date.now()
+  //       })
+  //       const NewPost = await postRepository.save(PostData)
+  //   expect(NewPost).not.toBeNull()
+  // }, 100000);
+
+  test('Find Non existing Post By ID returning error', async () => {
+    const nonExistingID = '1'
+    const WantedPost = await postRepository.findByID(nonExistingID)
+expect(WantedPost).toBeNull()
+}, 100000);
 });
