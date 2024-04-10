5a225553b59031c1e361bbc5766cb85f7652c6cd
JohnDias0 <dias44520@gmail.com>
2024-04-10 01:09:17 -0300

    feat: teste de deploy bem sucedido | API disponível em: https://boopeepapir.onrender.com

--- src/server.ts
+++ src/server.ts
@@ -9,25 +9,6 @@ const port = 3000
 app.use(express.json())
 app.use(router)
 
-// app.get('/usuarios', async (req: Request, res: Response) => {
-//   try {
-//     const collectionRef = admin.firestore().collection('users');
-//     const querySnapshot = await collectionRef.get();
-//     const users: User[] = [];
-//     querySnapshot.forEach((doc) => {
-//         const userData = doc.data() as User;
-//         users.push(userData);
-//     });
-//     if(users[1] === null){
-//         console.log('Nenhum Usuário encontrado')
-//         throw new Error('Nenhum Usuário encontrado')
-//     }
-//     res.json(users);
-// } catch (error) {
-//     console.error(`Error fetching users: ${error}`);
-// }
-//   });
-
 app.get('/', (req: Request, res: Response) => {
   res.send("<h1>Bem-vindo à API BOOPEEP</h1>")
 })
