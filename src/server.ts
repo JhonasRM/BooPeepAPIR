a508bca596551fd575560f224a7c5fc99bb8e18e
JohnDias0 <dias44520@gmail.com>
2024-04-10 01:06:30 -0300

    fix: sem tela de bem vindo :(

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,13 @@ import express, { Request, Response } from 'express';
 import router from "./Presentation/View/router";
 import app from "./Presentation/View/app";
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
-const path = require('path');
 const port = 3000
 
 app.use(express.json())
 app.use(router)
-app.use(express.static(path.join(__dirname, './Presentation/View')));
 
-app.get('/', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, './Presentation/View/index.html'));
-});
 // app.get('/usuarios', async (req: Request, res: Response) => {
 //   try {
 //     const collectionRef = admin.firestore().collection('users');
@@ -34,6 +28,9 @@ app.get('/', (req: Request, res: Response) => {
 // }
 //   });
 
+app.get('/', (req: Request, res: Response) => {
+  res.send("<h1>Bem-vindo à API BOOPEEP</h1>")
+})
 app.listen(port, () => {
     console.log(`O servidor está online na porta ${port}`)
 })
