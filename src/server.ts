7141ef87f9e966d1ce560a1b5c540cb9ae724a22
John Dias <120695464+JohnDias0@users.noreply.github.com>
2024-04-19 21:10:35 +0000

    feat: configuralções do cors no server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from 'express';
 import router from "./Presentation/View/router";
 import app from "./Presentation/View/app";
 import * as dotenv from 'dotenv';
+import { NextFunction } from 'express-serve-static-core';
 dotenv.config();
 
 const port = 3000
 
 app.use(express.json())
 app.use(router)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send("<h1>Bem-vindo à API BOOPEEP</h1>")
