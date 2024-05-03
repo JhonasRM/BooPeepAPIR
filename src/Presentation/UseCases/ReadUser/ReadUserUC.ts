5cc4a6f62cf3a170213b19c91c4e8b01199131c8
JohnDias0 <dias44520@gmail.com>
2024-05-03 16:14:00 -0300

    feat: ReadUserUC recode

--- src/Presentation/UseCases/ReadUser/ReadUserUC.ts
+++ src/Presentation/UseCases/ReadUser/ReadUserUC.ts
@@ -1,23 +1,24 @@
-
 import { User } from "../../../Service/Model/User";
 import { UsersRepository } from "../../../Service/Repositories/UsersRepository";
 import { IReadUserRequestDTO } from "./ReadUserDTO";
 export class ReadUserUC {
-    constructor(private usersRepository: UsersRepository) { }
-    async execute(data: IReadUserRequestDTO): Promise<{valido: boolean, value?:  number, erro?: string, data?: User }> {
-        try {
-            const wantedUser = await this.usersRepository.findByEmail(data.email)
-        if (wantedUser.valido === false) {
-            throw new Error(wantedUser.erro)
-        } else if( wantedUser.valido === true){
-                console.log('Usuário Logado com sucesso') 
-                
-            }
-            throw new Error('Senha Incorreta')
-        }
-        catch (error) {
-                return { valido: false, value: 500, erro: 'Internal Server Error'}
-            
-        }
-        
+  constructor(private usersRepository: UsersRepository) {}
+  async execute(
+    data: IReadUserRequestDTO
+  ): Promise<{ valido: boolean; value?: number; erro?: string; data?: User }> {
+    try {
+      const wantedUser = await this.usersRepository.findByEmail(data.email);
+      if (wantedUser.valido === false) {
+        return { valido: false, value: 404, erro: "Not Found" };
+      }
+      console.log("Usuario encontrado");
+      const user = wantedUser.value;
+      if (data.password !== user?.password) {
+        return { valido: false, value: 401, erro: "Unauthorized" };
+      }
+      return { valido: true, value: 201, data: user as User };
+    } catch (error) {
+      return { valido: false, value: 500, erro: "Internal Server Error" };
     }
+  }
+}
