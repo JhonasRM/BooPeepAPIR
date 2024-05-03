3f90a353c6f89680c444d61c3c0824daee2c96fc
JohnDias0 <dias44520@gmail.com>
2024-05-02 22:33:29 -0300

    fix: ReadUserUC

--- src/Presentation/UseCases/ReadUser/ReadUserUC.ts
+++ src/Presentation/UseCases/ReadUser/ReadUserUC.ts
@@ -1,19 +1,23 @@
 
+import { User } from "../../../Service/Model/User";
 import { UsersRepository } from "../../../Service/Repositories/UsersRepository";
 import { IReadUserRequestDTO } from "./ReadUserDTO";
 export class ReadUserUC {
     constructor(private usersRepository: UsersRepository) { }
-    async execute(data: IReadUserRequestDTO) {
-        const wantedUser = await this.usersRepository.findByEmail(data.email)
+    async execute(data: IReadUserRequestDTO): Promise<{valido: boolean, value?:  number, erro?: string, data?: User }> {
+        try {
+            const wantedUser = await this.usersRepository.findByEmail(data.email)
         if (wantedUser.valido === false) {
             throw new Error(wantedUser.erro)
-        } else {
-            console.log('Usuário Encontrado')
-            if( wantedUser.password === data.password){
+        } else if( wantedUser.valido === true){
                 console.log('Usuário Logado com sucesso') 
-                return wantedUser
+                
             }
             throw new Error('Senha Incorreta')
         }
+        catch (error) {
+                return { valido: false, value: 500, erro: 'Internal Server Error'}
+            
+        }
+        
     }
-}
