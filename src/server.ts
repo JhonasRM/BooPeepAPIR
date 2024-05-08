d7df1cb0c1b0b1ad6ef64daf312d4fbb686d9f38
John Dias <120695464+JohnDias0@users.noreply.github.com>
2024-05-08 15:40:14 +0000

    fix: CORS

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,21 @@ dotenv.config();
 
 const port = 3000
 
+const cors = require("cors");
+const corsOptions = {
+  origin: /\.onrender\.com$/,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+  
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(router)
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
+//app.use((req: Request, res: Response, next: NextFunction) => {
+  //res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //next();
+//})
 app.get('/', (req: Request, res: Response) => {
   res.send("<h1>Bem-vindo à API BOOPEEP</h1>")
 })
