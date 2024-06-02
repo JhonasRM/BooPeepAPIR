dd209544372c4092c164ac259e014425239ea572
John Dias <120695464+JohnDias0@users.noreply.github.com>
2024-06-02 00:23:59 -0300

    Atualizar o CreatePostUC.ts

--- src/Presentation/UseCases/Posts/CreatePost/CreatePostUC.ts
+++ src/Presentation/UseCases/Posts/CreatePost/CreatePostUC.ts
@@ -10,8 +10,8 @@ export class CreatePostUC {
     constructor(private postRepository: PostRepository, private usersFireStoreRepository: UsersFireStoreRepository, private usersAuthRepository: UsersAuthRepository) { }
     async execute(data: ICreatePostRequestDTO, email: string): Promise<IReturnAdapter>{
         try {
-            const userAuth = await this.usersAuthRepository.findByEmail(email)
-        if(userAuth.valido === false){
+            const userExists = await this.usersFireStoreRepository.findByIID(data.UserID)
+        if(userExists.valido === false){
             throw new Error('Usuário não encontrado')
         }
         const uid = userAuth.value?.uid as string
