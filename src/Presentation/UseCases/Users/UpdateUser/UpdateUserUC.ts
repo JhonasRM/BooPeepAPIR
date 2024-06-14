2c32758ac37f350361b5418e56ba7fc34ed85605
JohnDias0 <dias44520@gmail.com>
2024-06-14 16:56:01 -0300

    fix: UpdateUC

--- src/Presentation/UseCases/Users/UpdateUser/UpdateUserUC.ts
+++ src/Presentation/UseCases/Users/UpdateUser/UpdateUserUC.ts
@@ -24,7 +24,6 @@ export class UpdateUserUC {
             }
             const userData = userDataToUpdate.data as UserOnFirestore
             if (data.fieldToUpdate in user) {
-
                 const updatedUserAuth = await this.userAuthRepository.update(user.uid as string, data.fieldToUpdate, data.newValue)
                 if (updatedUserAuth.val === false) {
                     throw new Error(updatedUserAuth.erro)
