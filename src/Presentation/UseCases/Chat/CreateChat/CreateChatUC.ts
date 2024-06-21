2875c6536388b6ece1a425438f62cb33b5d3d014
JGomesV <140764535+JGomesV@users.noreply.github.com>
2024-06-21 04:36:43 +0000

    Feat: CreateChatUC e Model/Chat

--- src/Presentation/UseCases/Chat/CreateChat/CreateChatUC.ts
+++ src/Presentation/UseCases/Chat/CreateChat/CreateChatUC.ts
@@ -8,30 +8,35 @@ import { ICreateChatRequestDTO } from "./CreateChatDTO";
 export class CreateChatUC {
      constructor(private chatRepository: ChatRepository, private usersFireStoreRepository: UserFireStoreRepository) { }
      async execute(data: ICreateChatRequestDTO): Promise<IReturnAdapter> {
-         try {
-          const user = await this.usersFireStoreRepository.getUser(data.uid)
-          if (user.val === false) {
-              throw new Error('O Usuário não foi encontrado')
-          } 
-          const userData = user.data as UserOnFirestore
-          const uid = userData.uid
-          const field = 'chatID'
-          if(field in userData){
-           throw new Error ("O chat já existe")
+      try {
+        const user = await this.usersFireStoreRepository.getUser(data.uid)
+        if (user.val === false) {
+            throw new Error('O Usuário não foi encontrado')
+        } 
+        const userData = user.data as UserOnFirestore
+        const uid = userData.uid
+        const field = 'chatID'
+        let NewChat: Chat
+        if(userData.chatID !== ""){
+        const findChat = await this.chatRepository.readMessages(userData.chatID)
+        if(findChat.val === false){
+          throw new Error(findChat.erro)
         }
-          const NewChat: Chat = new Chat( uid as string)
-          console.log('Criando Novo Chat...')
-          const setchat = await this.chatRepository.setChat(NewChat)
-          if(setchat.val === false){
-            throw new Error(setchat.erro)  
-          }
-          const update = await this.usersFireStoreRepository.update(data.uid, 'chatID', NewChat.chatid)
-          if(update.val === false){
-            throw new Error('erro ao criar o chat para este usuário')
-          }
-          return {
-            val: true, data:setchat.data
-          }
+        NewChat = new Chat(uid, findChat.data, userData.chatID)
+      }
+        NewChat = new Chat( uid as string)
+        console.log('Criando Novo Chat...')
+        const setchat = await this.chatRepository.setChat(NewChat)
+        if(setchat.val === false){
+          throw new Error(setchat.erro)
+        }
+        const update = await this.usersFireStoreRepository.update(data.uid, 'chatID', NewChat.chatid)
+        if(update.val === false){
+          throw new Error('erro ao criar o chat para este usuário')
+        }
+        return {
+          val: true, data: NewChat
+        }
          } catch (error) {
           if(error instanceof Error){
             if(error.message === "O  chat já existe"){
