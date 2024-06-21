e5328cd2c6100404319fc8a14f8c37fc0e38d7b5
JhonasRM <129074169+JhonasRM@users.noreply.github.com>
2024-06-21 18:11:45 -0300

    feat: adaptação da CreatePostoController

--- src/Presentation/UseCases/Posts/CreatePost/CreatePostController.ts
+++ src/Presentation/UseCases/Posts/CreatePost/CreatePostController.ts
@@ -1,42 +1,43 @@
 import { Request, Response } from 'express';
 import { CreatePostUC } from './CreatePostUC';
 import { ICreatePostRequestDTO } from './CreatePostDTO';
+import { uploadImage } from '../services/uploadService';
 
 export class CreatePostController {
-    constructor(
-        private createPostUC: CreatePostUC,
-    ) { }
+    constructor(private createPostUC: CreatePostUC) {}
 
     async handle(request: Request, response: Response): Promise<void> {
-        const {
+        const { description, local, status, UserID } = request.body;
+        const file = request.file;
+
+        const PostData: ICreatePostRequestDTO = {
             description,
             local,
             status,
-            UserID
-        } = request.body;
+            UserID,
+            imageUrl: '',
+        };
 
-        const PostData: ICreatePostRequestDTO = {
-        description,
-            local,
-            status,
-            UserID
-        }
         try {
-            const createpost = await this.createPostUC.execute(PostData)
-            if(createpost.val === false){
-                throw new Error(createpost.erro as string)
+            if (file) {
+                PostData.imageUrl = await uploadImage(file);
+            }
+
+            const createpost = await this.createPostUC.execute(PostData);
+            if (createpost.val === false) {
+                throw new Error(createpost.erro as string);
             }
             response.status(201).send(createpost.data);
         } catch (error) {
-            if(error instanceof Error){
-                if(error.message === 'Usuário não encontrado'){
-                    response.status(404).send(error.message)
-                } else if(error.message !== 'Usuário não encontrado'){
-                    response.status(400).send(error.message)
+            if (error instanceof Error) {
+                if (error.message === 'Usuário não encontrado') {
+                    response.status(404).send(error.message);
+                } else if (error.message !== 'Usuário não encontrado') {
+                    response.status(400).send(error.message);
                 }
             } else {
-            response.status(500).send(`Internal Server Error: ${error}`)
-        }
+                response.status(500).send(`Internal Server Error: ${error}`);
+            }
         }
     }
 }
